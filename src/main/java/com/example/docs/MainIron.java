60c859447636307fa34f3b83bb3a8aba263bb447
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-18 14:30:22 +0300

    Iron Language 3.8 - Первый релиз

---
+++ src/main/java/com/example/docs/MainIron.java
@@ -0,0 +1,13 @@
+package com.example.docs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainIron {
+
+     @GetMapping("/iron")
+     public String getMethodName() {
+          return "documentation.html";
+     }
+}
